6a43fecc75291b52860a70e7cc042b61a650f889
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-05-09 19:06:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,4 +8,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  returns "It's ${days} days until ${holiday}!"
+  return `"It's ${days} days until ${holiday}!"`
+}
