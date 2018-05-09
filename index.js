ae5a90b105a36a138799e990d2c7ccd403472c82
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-05-09 19:10:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function happyHolidays() {
   return `Happy holidays!`
 }
 
-function happyHolidayTo(holiday, name) {
+function happyHolidaysTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
